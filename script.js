1d19203dca8adf101a021a51f25bb04cd1fc2ddd
lulu0306 <malumel03@gmail.com>
2019-05-29 10:19:20 -0500

    indented code

--- script.js
+++ script.js
@@ -3,8 +3,8 @@
 let square;
 for (var i = 0; i < 10; i++) {
 	for (var j = 0; j < 10; j++) {
-	square = `<div class="grid-item" data-row=${i} data-col=${j}>${i},${j}</div>`
-	$('.grid-container').append(square)	
+		square = `<div class="grid-item" data-row=${i} data-col=${j}>${i},${j}</div>`
+		$('.grid-container').append(square)	
 	}
 }
 //create an array of grid-item div
@@ -15,32 +15,25 @@ const squaresArray = $('.grid-item').toArray()
 
 //create unavalaible squares on the map 
 
-//randomly select number 
-
-
 function generateRandomlyNumber(){
-
-return Math.floor(Math.random()*10)
-
-
+	return Math.floor(Math.random()*10)
 }
-
 
 //declare an unavailable squares
 
 function generateBarriers(){
 	for(let i =0; i < 9; i++){
-	let randomX = generateRandomlyNumber();
-	let randomY = generateRandomlyNumber();
-	for(let j = 0; j < squaresArray.length; j++ ){
-		let statusX = parseInt(squaresArray[j].dataset.row)=== randomX;
-		let statusY = parseInt(squaresArray[j].dataset.col)=== randomY;
-		if (statusX && statusY ){
-			squaresArray[j].classList.add('unavailable');
-		}
+		let randomX = generateRandomlyNumber();
+		let randomY = generateRandomlyNumber();
+		for(let j = 0; j < squaresArray.length; j++ ){
+			let statusX = parseInt(squaresArray[j].dataset.row)=== randomX;
+			let statusY = parseInt(squaresArray[j].dataset.col)=== randomY;
+			if (statusX && statusY ){
+				squaresArray[j].classList.add('unavailable');
+			}
 
+		}
 	}
-}
 }
 
 generateBarriers()
